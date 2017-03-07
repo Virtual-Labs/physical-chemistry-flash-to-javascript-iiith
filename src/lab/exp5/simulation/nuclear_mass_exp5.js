6a5753a4f7ba38fbf45d261906731c5921f5a5d2
SravanthiModepu <sravanthimodepu@gmail.com>
2017-03-07 10:28:56 +0530

    modified the javascript simulation sources of exp5

--- src/lab/exp5/simulation/nuclear_mass_exp5.js
+++ src/lab/exp5/simulation/nuclear_mass_exp5.js
@@ -1,39 +1,87 @@
-var sol = ["benzene", "acetaldehyde", "ethyl"] ;
+var modal = { 
+        images: ['injector.png', 'acetaldehyde.png','below_arrow.png','benzene.png', 'blue-injector.png',
+        'chem_border.png','ethyl.png','green-injector.png', 'red-injector.png',]
 
-// function changeSolution() {
-//    if(sol == benzene){
-//       document.getElementById("benzene").style.visibility = "visible";
-      
+}
 
-//    else if(sol == acetaldehyde) {
-//       document.getElementById("acetaldehyde").style.visibility = "visible" ;
+  // onload function: call init method on window onload.
+    window.onload=function() {
+      view.init();
+      view.activateEvents();
+    }
 
-//    }
+var view ={
 
-//    else if(sol == ethyl) {
-//       document.getElementById("ethyl").style.visibility ="visibile"; 
-//    }
+  loadsamplecount: 0;
 
-// }
 
-// }
+  //showElements: calls this function to show the elements by its id.
+  showElements: function(id){
+    document.getElementById(id).style.visibility = 'visible' ;
+  },
 
-function benzene() {
+    // replaceElements: Calls this method to replace the element with an other element.
+  replaceElements: function(id, image) {
+    var element = document.getElementById(id);
+    element.src = image;
+  },
 
-   document.getElementById("red-injector").style.visibility = "visible";
-   var elem = document.getElementById("syringe");   
-      var pos = 0;
-      var id = setInterval(frame, 5);
-         function frame() {
-            if (pos == 345) {
-                clearInterval(id);
-                alert("Hello!");
-            } 
-            else {
-                pos++; 
-                  elem.style.left = pos + 'px'; 
-                  elem.style.left = pos + 'px'; 
-                  // alert("hello!!");
-             }
-         }
+  //addClcikEvent: calls this method to add EventListener to an element.
+  addClcikEvent : function(id, method){
+    var element = document.getElementById(id);
+    element.addEventListener('click',method);
+  },
+
+  //removeClickEvent: calls this method to remove EventListene for an element.
+  removeClickEvent: function(id, method){
+    document.getElementById(id).style.pointerEvents = 'none';
+  },
+
+  //enableClickEvent: Calls this method to enable EventListener for an element.
+    enableClickEvent: function(id){
+      document.getElementById(id).style.pointerEvents = 'auto';
+    },
+
+      //resetpage: calls this method to reset the page when we click the button.
+  resetpage: function() {
+    location.reload();
+  },
+
+      /* activateElements: calls this method to addEventListener to an elements, 
+    when an element is clicked corresponding funtioin gets executed. */
+  activateElements: function(id){
+    this.addClcikEvent('reset_button', function(){view.resetpage(); });
+    this.addClcikEvent('sample', function(){view.sample(); });
+    this.addClcikEvent('benzene', function(){view.benzene(); });
+    this.addClcikEvent('acetaldehyde,' function(){view.acetaldehyde(); });
+    this.addClcikEvent('ethyl', function(){view.ethyl(); });
+    this.addClcikEvent('loadsample', function(){view.loadsample(); });
+    this.addClcikEvent('start', function(){view.start(); });
+    this.addClcikEvent('plot', function(){view.plot(); });
+    // this.addClcikEvent('start', function(){view.start(); });
+    this.addClcikEvent('evaluate',function(){view.evaluate(); });
+  },
+
+  /*initialInstructions: calls this method to execute the following menthods:
+   1. when the page loads, this method displays the  */
+  initialInstructions: function(){
+    // this.setInnnerHTML('instruction', model.instruction1);
+    // this.removeClickEvent('caffeine'); 
+    this.removeClickEvent('loadsample');
+    this.removeClickEvent('start');
+    this.removeClickEvent('plot');
+    this.removeClickEvent('evaluate');
+    // this.removeClickEvent('IRdisc');
+    // this.removeClickEvent('start');
+    // this.removeClickEvent('evaluate');
+
+  },
+
+  sample: function() {
+    this.showElements('chem_border');
+    this.showElements('benzene');
+    this.showElements('acetaldehyde');
+    this.showElements('ethyl');
+  }
+
 }
